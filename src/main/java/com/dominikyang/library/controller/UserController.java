60dbc9ad33fa3021ae32981318220d788249abf0
Wei Yuyang <wyy453822487@outlook.com>
2020-06-23 04:06:46 +0800

    feat: add BCrypt demo

---
+++ src/main/java/com/dominikyang/library/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.dominikyang.library.controller;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Wei yuyaung
+ * @date 2020.06.23 4:01
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @PostMapping("add")
+    public String add(){
+        String username = "username";
+        String password = "password";
+        String hashPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+        return username + " " + hashPassword;
+    }
+
+}
